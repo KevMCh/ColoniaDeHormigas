5c9f4c07af6c2ab2d3f4d5982de2255f3b7795c5
Adrian <alu0100288216@ull.edu.es>
2015-10-30 13:53:35 +0000

    Modificados movimiento de la hormiga, añadido recogida de objetos, contador y texto de victoria.

---
+++ Colonia de Hormigas/Assets/Scripts/ObRecolecta.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class ObRecolecta : MonoBehaviour {
+
+	public GameObject objt;
+	
+	void Start () {
+
+		System.Random rnd = new System.Random(System.DateTime.Now.Millisecond);
+		
+		//float x = rnd.Next(1, 496);
+		//float y = rnd.Next(4, 496);
+		
+		//objt.transform.position = new Vector3(x, 2.5f, y);
+	
+	}
+
+	void Update (){
+		objt.transform.Rotate (new Vector3 (15, 30, 45) * Time.deltaTime);
+	}
+}
---
+++ Colonia de Hormigas/Assets/Scripts/playerController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class playerController : MonoBehaviour {
+	
+	public float speed;
+	public Text countText;
+	public Text winText;
+	
+	//private Rigidbody rb;
+	private int count;
+	
+	void Start (){
+		//rb = GetComponent<Rigidbody>();
+		count = 0;
+		setCountText ();
+		winText.text = "";
+	}
+	
+	void FixedUpdate (){
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		
+		//Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		//Vector3 movement = new Vector3 (moveHorizontal, 0.0f, 0.0f);
+		
+		//rb.AddForce (movement * speed);
+		this.transform.Translate ( Vector3.forward * moveVertical * speed);
+		this.transform.Rotate ( Vector3.up * moveHorizontal * speed* 2.0f);
+	}
+	
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.CompareTag ("Pick Up"))
+		{
+			other.gameObject.SetActive (false);
+			count = count +1;
+			setCountText();
+		}
+	}
+
+	void setCountText(){
+		countText.text = "Count: " + count.ToString();
+		if (count >= 12) {
+			winText.text = "You win!!";
+		}
+	}
+}
